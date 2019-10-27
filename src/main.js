dc28707c805aebd545df26469c2423fc580b5085
Hien Pham <hien.pham@unicorn-fintech.com>
2019-10-27 16:27:50 +0700

    create simple chat app with signalr client

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+// import './styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
